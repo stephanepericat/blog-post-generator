684d4f7d5fd6cc140b63fd9148f90817bfb7c314
Stephane P. Pericat <contact@pericat.net>
2025-08-18 21:22:30 -0500

    feat: setup cron job

--- api/generate/index.ts
+++ api/generate/index.ts
@@ -3,8 +3,16 @@ import mockSearchResults from '../../assets/search-results.js'
 
 export async function GET(req: Request) {
   const isDev = process.env.NODE_ENV === 'development'
+
+  // Get web search results
   const query = 'Covid-19'
   const data = isDev ? mockSearchResults : await braveSearch(query)
+
+  // Sent to LLM for summarization
+
+  // Post draft to Sanity
+
+  // Email draft to users
 
   return new Response(JSON.stringify(data), {
     headers: { 'Content-Type': 'application/json' },
